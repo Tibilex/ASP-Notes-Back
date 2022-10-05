9f78f4c07890a8c83a1c171c52d42cd1fc56bc16
Tibilex <45212175+Tibilex@users.noreply.github.com>
2022-10-05 21:58:30 +0300

    Add: "Note" repository

---
+++ QueryLibrary/Repositories/NoteRepository.cs
@@ -0,0 +1,48 @@
+﻿using Dapper.Contrib.Extensions;
+using QueryLibrary.Models;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace QueryLibrary.Repositories
+{
+    public class NoteRepository
+    {
+        private static string _connectionString = "Server=DESKTOP-440KLQF\\SQLEXPRESS;Database=ASP_DataBase;Trusted_Connection=True;";
+
+        // Get All Notes
+        public static IEnumerable<Note> GetAll()
+        {
+            using (IDbConnection connection = new SqlConnection(_connectionString))
+            {
+                return connection.GetAll<Note>();
+            }
+        }
+
+        // Get Note for "Id"
+        public static Note GetNoteToId(int id)
+        {
+            using (IDbConnection connection = new SqlConnection(_connectionString))
+            {
+                return connection.Get<Note>(id);
+            }
+        }
+
+        // Add Note
+        public static long AddNote(Note note)
+        {
+            using (IDbConnection connection = new SqlConnection(_connectionString))
+            {
+                return connection.Insert(note);
+            }
+        }
+
+        // Delete Note
+        public static bool DeleteNote(int id)
+        {
+            using (IDbConnection connection = new SqlConnection(_connectionString))
+            {
+                return connection.Delete<Note>(new Note { Id = id });
+            }
+        }
+    }
+}
