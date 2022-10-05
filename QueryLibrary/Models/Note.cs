36e91101c3968d6ddf7e28890672eebc8d67534b
Tibilex <45212175+Tibilex@users.noreply.github.com>
2022-10-05 19:58:28 +0300

    Add: model "Note"

---
+++ QueryLibrary/Models/Note.cs
@@ -0,0 +1,10 @@
+﻿namespace QueryLibrary.Models
+{
+    public class Note
+    {
+        public string Owner { get; set; }
+        public string Label { get; set; }
+        public string Text { get; set; }
+        public DateTime CreatedTime { get; set; }
+    }
+}
