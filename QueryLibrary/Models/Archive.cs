ec3db793a2b64ae619cf4944d0f883506cb69f5d
Tibilex <45212175+Tibilex@users.noreply.github.com>
2022-10-05 20:11:01 +0300

    Add: Model "Archive"

---
+++ QueryLibrary/Models/Archive.cs
@@ -0,0 +1,10 @@
+﻿namespace QueryLibrary.Models
+{
+    public class Archive
+    {
+        public string Owner { get; set; }
+        public string Label { get; set; }
+        public string Text { get; set; }
+        public DateTime AddToArchive { get; set; }
+    }
+}
---
+++ QueryLibrary/Repositories/UserRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueryLibrary.Repositories
+{
+    public class UserRepository
+    {
+        private string _connectionString = "Server=DESKTOP-440KLQF\\SQLEXPRESS;Database=ASP_DataBase;Trusted_Connection=True;";
+
+
+    }
+}
